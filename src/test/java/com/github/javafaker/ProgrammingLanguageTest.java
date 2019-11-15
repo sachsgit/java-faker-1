7b09bbfd8678b5618966987c6494ac11f34c4489
SS007979 <Shaule.Sachs@penfed.org>
2019-11-15 11:35:29 -0500

    Import clean-up in ProgrammingLanugageTest.java

--- src/test/java/com/github/javafaker/ProgrammingLanguageTest.java
+++ src/test/java/com/github/javafaker/ProgrammingLanguageTest.java
@@ -1,10 +1,9 @@
 package com.github.javafaker;
 
-import com.github.javafaker.repeating.Repeat;
-import org.junit.Test;
-
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
 
 public class ProgrammingLanguageTest extends AbstractFakerTest {
 
