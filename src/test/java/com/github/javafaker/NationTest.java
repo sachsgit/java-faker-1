8ad009b3a71777bc1071d2b87f773c5fea31ba69
sachsgit <sachs@atlantisuniverse.com>
2019-12-25 19:54:57 -0500

    replace assertThat with hamcrest

--- src/test/java/com/github/javafaker/NationTest.java
+++ src/test/java/com/github/javafaker/NationTest.java
@@ -1,9 +1,9 @@
 package com.github.javafaker;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
